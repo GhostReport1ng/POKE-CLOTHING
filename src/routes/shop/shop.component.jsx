362ed880825d898d3b77cdbb53dc25b8406dfc58
GhostReport1ng <pastrulator@hotmail.com>
2022-08-04 23:07:04 +0200

    routes added

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,12 @@
+const Shop  = () => {
+    return (
+      <div>
+        <div>
+          <h1>I'm the shop</h1>
+        </div>
+      </div>
+    )
+  }
+
+
+export default Shop;
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+
+const SignIn = () => {
+    return (
+        <Fragment>
+            <div>
+                <h1>Sign in Page</h1>
+            </div>
+        </Fragment>
+    )
+}
+
+export default SignIn
