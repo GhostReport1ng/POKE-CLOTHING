b38fad053e623a2ba616058f8935bd67355c9102
unknown <pastrulator@hotmail.com>
2022-11-15 14:26:55 +0100

    svašta nešto dodato

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,34 @@
 // import SHOP_DATA from '../../shop-data.json'
-import { useContext } from 'react'
-import { ProductsContext } from '../../components/contexts/products.context'
+import { useContext, Fragment } from 'react'
+import { CategoriesContext } from '../../components/contexts/categories.context'
 import ProductCard from '../../components/product-card/product-card.component'
 import './shop.styles.scss'
 
-const Shop  = () => {
+const Shop = () => {
 
-    const {products} = useContext(ProductsContext)
+    const { categoriesMap } = useContext(CategoriesContext)
     return (
-      <div className='products-container'>
-        {products.map((product) => (
-            <ProductCard key={product.id} product={product}></ProductCard>
-        ))}
-      </div>
+
+      <Fragment>
+      
+        {
+          Object.keys(categoriesMap).map((title) => (
+            <Fragment key={title}>
+              <h2>
+                {title}
+              </h2>
+              <div className='products-container'>
+                {categoriesMap[title].map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+              </div>
+            </Fragment>
+          ))
+
+        }
+      
+
+      </Fragment>
     )
   }
 
